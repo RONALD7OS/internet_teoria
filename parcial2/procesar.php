82c3867ca88907d9a93992acc806343d9274951c
Ronald <theronald78342788@gmail.com>
2026-08-11 20:28:26 -0400

    todo

--- parcial2/procesar.php
+++ parcial2/procesar.php
@@ -4,15 +4,20 @@ $correo = $_POST["correo"];
 $mensaje = $_POST["consulta"];
 
 echo "<h2>Cita reservada en Óptica Mirasol</h2>";
+
 echo "<p>Nombre: $nombre</p>";
 echo "<p>Correo: $correo</p>";
 echo "<p>Mensaje: $mensaje</p>";
 
-foreach ($servicios as $servicio) {
-    echo "<p>Servicio: $servicio</p>";
-}
+echo "<h3>Servicios :</h3>";
+
 $servicios = [
-    "Examen de vista - Bs 50",
+    "examen de vista - Bs 50",
     "Armazón clásico - Bs 180",
     "Lentes de sol - Bs 120"
 ];
+
+foreach ($servicios as $servicio) {
+    echo "<p>$servicio</p>";
+}
+
